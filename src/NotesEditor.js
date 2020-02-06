b3127c31e02d3a522b682e490870f364125111e8
Casey Fayard <fayardcasey@gmail.com>
2020-02-06 11:01:28 -0500

    saves to NotesApp on submit from NoteEditor

--- src/NotesEditor.js
+++ src/NotesEditor.js
@@ -4,24 +4,28 @@ class NoteEditor extends React.Component {
 
     constructor(props) {
         super(props);
-            console.log('i am the note editor constructor')
+
+        console.log('I am the NoteEditor constructor!!!!!!!');
         this.state = {
             changedNote: {}
         }
     }
-    
-    // receiving the new proops and the eisting state
-    static getDerivedStateFromProps(props, state) {
-        console.log('We are in getDerivedStateFromProps')
 
-        console.table(props.note)
-        console.table(state.changedNote)
+    // We receive the new props and the existing state
+    static getDerivedStateFromProps(props, state) {
+        // static means there is not `this` keyword
+
+        console.log('We are in getDerivedStateFromProps');
+
+        console.table(props.note);
+        console.table(state.changedNote);
 
         if (props.note.id === state.changedNote.id) {
             return {
                 ...state
             }
         } else {
+            // We return the new version of state!
             return {
                 ...state,
                 changedNote: {
@@ -30,43 +34,47 @@ class NoteEditor extends React.Component {
             }
         }
 
-
-        // We return the new version of state.
-       
     }
-
+    
     render() {
         return (
             <div>
-                <input 
-                    value={this.state.changedNote.title} 
-                    onChange={(event) => {
-                        this._updateLocalNote({
-                            ...this.state.changedNote,
-                            title: event.target.value
-                        });
-                    }}
-                />
-                <br />
-                <textarea 
-                    value={this.state.changedNote.copy} 
-                    onChange={(event) => {
-                        this._updateLocalNote({
-                            ...this.state.changedNote,
-                            copy: event.target.value
-                        });
-                    }}
-                />
+                <form onSubmit={this._handleSubmit}>
+                    <input 
+                        value={this.state.changedNote.title} 
+                        onChange={(event) => {
+                            this._updateLocalNote({
+                                ...this.state.changedNote,
+                                title: event.target.value
+                            });
+                        }}
+                    />
+                    <br />
+                    <textarea 
+                        value={this.state.changedNote.copy} 
+                        onChange={(event) => {
+                            this._updateLocalNote({
+                                ...this.state.changedNote,
+                                copy: event.target.value
+                            });
+                        }}
+                    />
+                    <button>Save</button>                    
+                </form>
             </div>
         );
+    }
+
+    _handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.handleChange(this.state.changedNote);
     }
 
     _updateLocalNote = (changedNote) => {
         this.setState({
             changedNote
-        })
+        });
     }
-
 }
 
 export default NoteEditor;
